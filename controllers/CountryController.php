d9402e6d46e9c91ccab515992e430b5fbd58f742
Day <daianrene99@gmail.com>
2021-10-01 18:20:10 -0300

    First commit

---
+++ controllers/CountryController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Country;
+use app\models\CountrySearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * CountryController implements the CRUD actions for Country model.
+ */
+class CountryController extends Controller
+{
+
+    /**
+     * Lists all Country models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new CountrySearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionDuplicados()
+    {
+        $searchModel = new CountrySearch();
+        $dataProvider = $searchModel->searchDuplicado($this->request->queryParams);
+
+        
+
+        return $this->render('duplicados', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+    /**
+     * Finds the Country model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $code Code
+     * @return Country the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($code)
+    {
+        if (($model = Country::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
